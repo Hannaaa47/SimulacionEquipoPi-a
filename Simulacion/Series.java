50d9912c3729b9d8b375771304e90d65d14d95da
Hanna Amador <152832888+Hannaaa47@users.noreply.github.com>
2025-03-31 09:15:43 +0000

    Seirees me caes mal

--- Simulacion/Series.java
+++ Simulacion/Series.java
@@ -1,9 +1,10 @@
 package Simulacion;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Series
 {
-    public void SeriesMain(String rutaArchivo, double tablaChi[]) {
+    public void SeriesMain(String rutaArchivo, double tablaChi[], Scanner sc) {
         // Leer archivo usando ManipularCSV
         ManipularCSV csv = new ManipularCSV();
         csv.leerArchivo(rutaArchivo);
@@ -11,7 +12,10 @@ public class Series
         ArrayList<Double> valores = csv.ListaValores();
         
         // Realizar prueba de series
-        PruebaSerie prueba = new PruebaSerie(valores);
+        System.out.print("Ingrese n: ");
+        int n= sc.nextInt();
+        System.out.println();
+        PruebaSerie prueba = new PruebaSerie(valores,n);
         prueba.formarPares();
         prueba.mostrarResultados(tablaChi);
     }
@@ -22,11 +26,14 @@ public class Series
         private int n = 5; // Número de intervalos
         private int[][] oij; 
         private double Eij;
+        private double[][] tabEij;
 
-        public PruebaSerie(ArrayList<Double> valores) {
+        public PruebaSerie(ArrayList<Double> valores, int n) {
             this.valores = valores;
             this.N = valores.size();
+            this.n= n;
             this.oij = new int[n][n];
+            this.tabEij = new double[n][n];
             this.Eij = (double)N / (n * n); // Calcula la frecuencia esperada
         }
 
@@ -37,70 +44,143 @@ public class Series
             for (int i = 0; i < N; i++) { 
                 //forma pares
                 ui = valores.get(i);
-                if (i == N-1){
-                    ui1 = valores.get(0);
+                if (i == 0){
+                    ui1 = valores.get(N-1);
                 } else {
-                    ui1 = valores.get(i + 1);
+                    ui1 = valores.get(i - 1);
                 }
 
                 //los acomoda en los obvservados
-                int fila = (int) (ui * n);
-                int columna = (int) (ui1 * n);
+                int fila=-1;
+                int columna=-1;
+       
 
-                // Ajustar para valores iguales a 1.0
-                if (fila == n) fila--;
-                if (columna == n) columna--;
+                double x;
+                double y;
+                for (int j = 0; j < n && columna==-1; j++) {
+                    x = (j + 1) / (double) n;
+                    if(ui1 <= x) columna=j;
+                }
+
+                for (int k = 0;  k< n && fila==-1; k++) {
+                    y = (k + 1) / (double) n;
+                    if(ui <= y) fila=k;
+                }
+                if (fila==-1) fila=n-1;
+                if (columna==-1) columna=n-1;
 
                 oij[fila][columna]++;
             }
         }
 
-        public double calcularChiCuadrado() //METODO QUE USA LA FORMULA
-        {
-            double chi2 = 0.0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    double diferencia = oij[i][j] - Eij;
-                    chi2 += (diferencia * diferencia) / Eij;
-                }
+        public double calcularChiCuadrado()  {
+            double chi2 = 0.0,temp, diferencia;
+            System.out.println("Tabla de frecuencias observadas (Oij - Eij)² / Eij:");
+            // Encabezado de columnas
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.print("|            | ");
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%-10.2f | ", (j + 1) / (double) n);
             }
+            System.out.println();
+            System.out.println("-------------------------------------------------------------------------------");
+            for (int i = n - 1; i >= 0; i--) { // Cambio aquí: iterar en orden inverso
+                System.out.printf("| %-10.2f | ", (i + 1) / (double) n);
+                for (int j = 0; j < n; j++) { 
+                    diferencia = tabEij[i][j];
+                    temp= (diferencia * diferencia) / Eij;
+                    chi2 += temp;    
+                    System.out.printf("%-10.4f | ", temp );
+                }
+                System.out.println();
+            }
+            System.out.println("-------------------------------------------------------------------------------");
             return chi2;
         }
 
         public void mostrarResultados(double []tablaChi) {
             // Mostrar tabla Oij
             System.out.println("Tabla de frecuencias observadas (Oij):");
-            System.out.println("-----------------------------------");
             
+            // tabla oij
             // Encabezado de columnas
-            System.out.print("     ");
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.print("|            | ");
             for (int j = 0; j < n; j++) {
-                System.out.printf("%.1f  ", (j + 1) / (double) n);
+                System.out.printf("%-10.2f | ", (j + 1) / (double) n);
             }
             System.out.println();
-            
-            // Filas de la tabla (orden inverso)
+            System.out.println("-------------------------------------------------------------------------------");
+
             for (int i = n - 1; i >= 0; i--) { // Cambio aquí: iterar en orden inverso
-                System.out.printf("%.1f | ", (i + 1) / (double) n);
+                System.out.printf("| %-10.2f | ", (i + 1) / (double) n);
                 for (int j = 0; j < n; j++) {
-                    System.out.printf("%3d ", oij[i][j]);
+                    
+                    System.out.printf("%-10d | ", oij[i][j]);
                 }
                 System.out.println();
             }
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println();
+
+            // tabla eij
+            System.out.println("Tabla de frecuencias observadas (Eij):");
+            // Encabezado de columnas
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.print("|            | ");
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%-10.2f | ", (j + 1) / (double) n);
+            }
+            System.out.println();
+            System.out.println("-------------------------------------------------------------------------------");
+
+            for (int i = n - 1; i >= 0; i--) { // Cambio aquí: iterar en orden inverso
+                System.out.printf("| %-10.2f | ", (i + 1) / (double) n);
+                for (int j = 0; j < n; j++) {
+                    
+                    System.out.printf("%-10.4f | ", Eij);
+                }
+                System.out.println();
+            }
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println();
+
+            // tabla oij - eij
+            System.out.println("Tabla de frecuencias observadas (Oij - Eij):");
+            // Encabezado de columnas
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.print("|            | ");
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%-10.2f | ", (j + 1) / (double) n);
+            }
+            System.out.println();
+            System.out.println("-------------------------------------------------------------------------------");
+ 
+            for (int i = n - 1; i >= 0; i--) { // Cambio aquí: iterar en orden inverso
+                System.out.printf("| %-10.2f | ", (i + 1) / (double) n);
+                for (int j = 0; j < n; j++) {    
+                    tabEij[i][j] = (double)oij[i][j] - Eij;  
+                    System.out.printf("%-10.4f | ", tabEij[i][j] );
+                }
+                System.out.println();
+            }
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println();
+
             // Calcular y mostrar X²
             double chi2 = calcularChiCuadrado(); //x calculado
             double in =Math.pow(n, 2) - 1;
             System.out.println("\nResultados de la prueba:");
             System.out.println("N = " + N);
             System.out.println("n = " + n);
-            System.out.printf("X2 calculado = %.4f\n", chi2);
-            System.out.println("X2 crítico (" + in  + " 5%) = " +  tablaChi[(int)in-1]); //x tabla 
+            System.out.printf("X² calculado = %.4f\n", chi2);
+            System.out.println("X² crítico (" + in  + " 5%) = " +  tablaChi[(int)in-1]); //x tabla 
             
             // Conclusión
             if (chi2 < tablaChi[(int)n-1]) {
-                System.out.println("Conclusión: X2 < X²(" + in  + ", 5%) → Se acepta H0");
+                System.out.println("Conclusión: X² < X²(" + in  + ", 5%) → Se acepta H0");
             } else {
-                System.out.println("Conclusión: X2 ≥ X²(" + in  + ", 5%) → Se rechaza H0");
+                System.out.println("Conclusión: X² ≥ X²(" + in  + ", 5%) → Se rechaza H0");
             }
         }
     }
